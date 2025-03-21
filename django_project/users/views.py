9c6f1bd297d1d22c939d640f35f6c36ecde11447
Bhavya <bhavyaky0690@gmail.com>
2025-03-21 10:53:19 +0530

    Working on User Registration.

---
+++ django_project/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+# Create your views here.
